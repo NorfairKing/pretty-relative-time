21b29c9d729ed91a56819f569de6fdc8582d7e3d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-09-07 18:33:07 +0200

    little fix for older ghc versions

--- src/Text/Time/Pretty/Render.hs
+++ src/Text/Time/Pretty/Render.hs
@@ -12,23 +12,19 @@ renderDaysAgoAuto :: DaysAgo -> String
 renderDaysAgoAuto DaysAgo {..} =
   case daysAgoSign of
     GT ->
-      if | daysAgoYears > 0 ->
-           unwords [ plural daysAgoYears "year" "years", "ago"]
-         | daysAgoMonths > 0 ->
-           unwords [ plural daysAgoMonths "month" "months", "ago"]
-         | daysAgoWeeks > 0 ->
-           unwords [ plural daysAgoWeeks "week" "weeks", "ago"]
+      if | daysAgoYears > 0 -> unwords [plural daysAgoYears "year" "years", "ago"]
+         | daysAgoMonths > 0 -> unwords [plural daysAgoMonths "month" "months", "ago"]
+         | daysAgoWeeks > 0 -> unwords [plural daysAgoWeeks "week" "weeks", "ago"]
          | daysAgoDays == 1 -> "yesterday"
-         | daysAgoDays > 0 -> unwords [ plural daysAgoDays "day" "days", "ago"]
+         | daysAgoDays > 0 -> unwords [plural daysAgoDays "day" "days", "ago"]
          | otherwise -> "today"
     EQ -> "today"
     LT ->
-      if | daysAgoYears > 0 -> unwords ["in",  plural daysAgoYears "year" "years"]
-         | daysAgoMonths > 0 ->
-           unwords ["in",  plural daysAgoMonths "month" "months"]
-         | daysAgoWeeks > 0 -> unwords ["in",  plural daysAgoWeeks "week" "weeks"]
+      if | daysAgoYears > 0 -> unwords ["in", plural daysAgoYears "year" "years"]
+         | daysAgoMonths > 0 -> unwords ["in", plural daysAgoMonths "month" "months"]
+         | daysAgoWeeks > 0 -> unwords ["in", plural daysAgoWeeks "week" "weeks"]
          | daysAgoDays == 1 -> "tomorrow"
-         | daysAgoDays > 0 -> unwords ["in",  plural daysAgoDays "day" "days"]
+         | daysAgoDays > 0 -> unwords ["in", plural daysAgoDays "day" "days"]
          | otherwise -> "today"
 
 renderTimeAgoAuto :: TimeAgo -> String
@@ -53,5 +49,5 @@ renderTimeAgoAuto TimeAgo {..} =
          | otherwise -> "just now"
 
 plural :: (Show a, Integral a) => a -> String -> String -> String
-plural 1 sing _ = "1 " <> sing
-plural n _ plur = show n <> " " <> plur
+plural 1 sing _ = "1 " ++ sing
+plural n _ plur = show n ++ " " ++ plur
