e2956e93752eba624bcd546d0f9ef562acf185ea
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-03-06 18:03:27 +0100

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   description = "pretty-relative-time";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     validity.url = "github:NorfairKing/validity?ref=flake";
     validity.flake = false;
@@ -21,67 +20,66 @@
     , pre-commit-hooks
     , validity
     }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ]
-      (system:
-      let
-        pkgsFor = nixpkgs: import nixpkgs {
-          inherit system;
-          overlays = [
-            self.overlays.${system}
-            (import (validity + "/nix/overlay.nix"))
-          ];
-        };
-        pkgs = pkgsFor nixpkgs;
-      in
-      {
-        overlays = import ./nix/overlay.nix;
-        packages.default = pkgs.haskellPackages.pretty-relative-time;
-        checks =
-          let
-            backwardCompatibilityCheckFor = nixpkgs:
-              let pkgs' = pkgsFor nixpkgs;
-              in pkgs'.haskellPackages.pretty-relative-time;
-            allNixpkgs = {
-              inherit
-                nixpkgs-22_05
-                nixpkgs-21_11
-                nixpkgs-21_05;
-            };
-            backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
-          in
-          backwardCompatibilityChecks // {
-            pre-commit = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                hlint.enable = true;
-                hpack.enable = true;
-                ormolu.enable = true;
-                nixpkgs-fmt.enable = true;
-                nixpkgs-fmt.excludes = [ ".*/default.nix" ];
-                cabal2nix.enable = true;
-              };
+    let
+      system = "x86_64-linux";
+      pkgsFor = nixpkgs: import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.${system}
+          (import (validity + "/nix/overlay.nix"))
+        ];
+      };
+      pkgs = pkgsFor nixpkgs;
+    in
+    {
+      overlays.${system} = import ./nix/overlay.nix;
+      packages.${system}.default = pkgs.haskellPackages.pretty-relative-time;
+      checks.${system} =
+        let
+          backwardCompatibilityCheckFor = nixpkgs:
+            let pkgs' = pkgsFor nixpkgs;
+            in pkgs'.haskellPackages.pretty-relative-time;
+          allNixpkgs = {
+            inherit
+              nixpkgs-22_05
+              nixpkgs-21_11
+              nixpkgs-21_05;
+          };
+          backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
+        in
+        backwardCompatibilityChecks // {
+          pre-commit = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              hlint.enable = true;
+              hpack.enable = true;
+              ormolu.enable = true;
+              nixpkgs-fmt.enable = true;
+              nixpkgs-fmt.excludes = [ ".*/default.nix" ];
+              cabal2nix.enable = true;
             };
           };
-        devShells.default = pkgs.haskellPackages.shellFor {
-          name = "pretty-relative-time-shell";
-          packages = (p:
-            [ p.pretty-relative-time ]
-          );
-          withHoogle = true;
-          doBenchmark = true;
-          buildInputs = with pkgs; [
-            niv
-            zlib
-            cabal-install
-          ] ++ (with pre-commit-hooks;
-            [
-              hlint
-              hpack
-              nixpkgs-fmt
-              ormolu
-              cabal2nix
-            ]);
-          shellHook = self.checks.${system}.pre-commit.shellHook;
         };
-      });
+      devShells.${system}.default = pkgs.haskellPackages.shellFor {
+        name = "pretty-relative-time-shell";
+        packages = (p:
+          [ p.pretty-relative-time ]
+        );
+        withHoogle = true;
+        doBenchmark = true;
+        buildInputs = with pkgs; [
+          niv
+          zlib
+          cabal-install
+        ] ++ (with pre-commit-hooks;
+          [
+            hlint
+            hpack
+            nixpkgs-fmt
+            ormolu
+            cabal2nix
+          ]);
+        shellHook = self.checks.${system}.pre-commit.shellHook;
+      };
+    };
 }
