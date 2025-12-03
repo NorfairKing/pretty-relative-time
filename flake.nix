e83189c875d4b8e6d302f14e88077c0be99f9c83
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-12-03 10:46:47 +0100

    Upgrade to nixos-25.11 and lts-24.16

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "pretty-relative-time";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
+    nixpkgs-25_05.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
     nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
     nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
@@ -10,6 +11,7 @@
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-25_05
     , nixpkgs-24_11
     , nixpkgs-24_05
     , pre-commit-hooks
@@ -34,6 +36,7 @@
             in pkgs'.haskellPackages.pretty-relative-time;
           allNixpkgs = {
             inherit
+              nixpkgs-25_05
               nixpkgs-24_11
               nixpkgs-24_05;
           };
